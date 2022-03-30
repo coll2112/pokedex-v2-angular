98ed268978422e87630a28c3b8215860481f0200
coll2112 <lgc9312@gmail.com>
2022-03-30 18:45:51 -0500

    added routing with new details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DetailsComponent } from './components/details/details.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
+  { path: 'pokedex', component: PokemonComponent },
+  { path: 'pokedex/:name', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent, PokemonComponent],
+  declarations: [AppComponent, PokemonComponent, DetailsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
