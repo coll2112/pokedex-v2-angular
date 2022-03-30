b35135ad5e37fbbf4b7ff5588f8d91e99e702e0c
coll2112 <lgc9312@gmail.com>
2022-03-30 18:59:09 -0500

    retrieved data for specific pokemon in details component

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from 'src/app/services/pokemon.model';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  pokemonDetails: Pokemon = {
+    name: '',
+    id: 0,
+    sprites: {
+      front_default: '',
+      front_shiny: '',
+      back_default: '',
+      back_shiny: '',
+    },
+    types: [],
+    weight: 0,
+    height: 0,
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) =>
+      this.pokemonService
+        .getPokemonDetails(params['name'])
+        .subscribe((data) => (this.pokemonDetails = data))
+    );
   }
-
 }
